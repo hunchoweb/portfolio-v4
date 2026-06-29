233705ada3f5c5808c595b7444a05dee3e986a3b
hunchoweb <hunchoweb@gmail.com>
2026-06-29 17:23:51 +0100

    Refactor Work component imports to remove unused dependencies

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
 import SectionHeader from "./SectionHeader";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
